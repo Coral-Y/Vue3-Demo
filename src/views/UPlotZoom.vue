d983df3d762e58952fa3b1e650ce8f53011070e9
coral <coralyang@dh-data.com>
2024-03-28 15:35:26 +0800

    滚轮左右平移

--- src/views/UPlotZoom.vue
+++ src/views/UPlotZoom.vue
@@ -38,22 +38,28 @@ function wheelZoomPlugin(opts) {
   return {
     hooks: {
       ready: (u) => {
-        xMin = u.scales.x.min
-        xMax = u.scales.x.max
-        yMin = u.scales.y.min
-        yMax = u.scales.y.max
+        xMin = u.scales.x.min // x轴的最小值
+        xMax = u.scales.x.max // x轴的最大值
+        yMin = u.scales.y.min // y轴的最小值
+        yMax = u.scales.y.max // y轴的最大值
 
-        xRange = xMax - xMin
-        yRange = yMax - yMin
+        xRange = xMax - xMin // x轴数据范围
+        yRange = yMax - yMin // y轴数据范围
 
+        console.log(xMin, xMax, yMin, yMax, xRange, yRange)
+
+        //.u-over dom element
         let over = u.over
+        // 返回一个DOMRect对象，该对象是一个将范围内所有元素的边界矩形包围起来的矩形。用于确认文本区域中选中部分或光标的视窗坐标
         let rect = over.getBoundingClientRect()
 
         let left0
         let scXMin0 = u.scales.x.min
         let scXMax0 = u.scales.x.max
 
+        // posToVal：将相对于绘图区域的CSS像素位置转换为沿给定比例的值
         let xUnitsPerPx = u.posToVal(1, 'x') - u.posToVal(0, 'x')
+        console.log(`xUnitsPerPx: ${xUnitsPerPx}`)
 
         function onmove(e) {
           e.preventDefault()
@@ -62,14 +68,16 @@ function wheelZoomPlugin(opts) {
           //	let top1 = e.clientY;
 
           let dx = xUnitsPerPx * (left1 - left0)
+          console.log(`dx: ${dx}`)
 
+          // 设置比例限制及重绘
           u.setScale('x', {
             min: scXMin0 - dx,
             max: scXMax0 - dx
           })
         }
 
-        function onup(e) {
+        function onup() {
           document.removeEventListener('mousemove', onmove)
           document.removeEventListener('mouseup', onup)
         }
@@ -90,37 +98,51 @@ function wheelZoomPlugin(opts) {
         // wheel scroll zoom
         over.addEventListener('wheel', (e) => {
           e.preventDefault()
+          let scXMin0 = u.scales.x.min
+          let scXMax0 = u.scales.x.max
 
-          let { left, top } = u.cursor
+          console.log(e.deltaY)
 
-          let leftPct = left / rect.width
-          let btmPct = 1 - top / rect.height
-          let xVal = u.posToVal(left, 'x')
-          let yVal = u.posToVal(top, 'y')
-          let oxRange = u.scales.x.max - u.scales.x.min
-          let oyRange = u.scales.y.max - u.scales.y.min
-
-          let nxRange = e.deltaY < 0 ? oxRange * factor : oxRange / factor
-          let nxMin = xVal - leftPct * nxRange
-          let nxMax = nxMin + nxRange
-          ;[nxMin, nxMax] = clamp(nxRange, nxMin, nxMax, xRange, xMin, xMax)
-
-          let nyRange = e.deltaY < 0 ? oyRange * factor : oyRange / factor
-          let nyMin = yVal - btmPct * nyRange
-          let nyMax = nyMin + nyRange
-          ;[nyMin, nyMax] = clamp(nyRange, nyMin, nyMax, yRange, yMin, yMax)
-
-          u.batch(() => {
-            u.setScale('x', {
-              min: nxMin,
-              max: nxMax
-            })
-
-            u.setScale('y', {
-              min: nyMin,
-              max: nyMax
-            })
+          let dx = 0.01 * e.deltaY
+          // 设置比例限制及重绘
+          u.setScale('x', {
+            min: scXMin0 - dx,
+            max: scXMax0 - dx
           })
+
+          // 光标状态和选项
+          // let { left, top } = u.cursor
+          // console.log(left, top)
+
+          // let leftPct = left / rect.width
+          // let btmPct = 1 - top / rect.height
+          // let xVal = u.posToVal(left, 'x')
+          // let yVal = u.posToVal(top, 'y')
+          // let oxRange = u.scales.x.max - u.scales.x.min
+          // let oyRange = u.scales.y.max - u.scales.y.min
+
+          // let nxRange = e.deltaY < 0 ? oxRange * factor : oxRange / factor
+          // let nxMin = xVal - leftPct * nxRange
+          // let nxMax = nxMin + nxRange
+          // ;[nxMin, nxMax] = clamp(nxRange, nxMin, nxMax, xRange, xMin, xMax)
+
+          // let nyRange = e.deltaY < 0 ? oyRange * factor : oyRange / factor
+          // let nyMin = yVal - btmPct * nyRange
+          // let nyMax = nyMin + nyRange
+          // ;[nyMin, nyMax] = clamp(nyRange, nyMin, nyMax, yRange, yMin, yMax)
+
+          // // 手动批处理多个操作
+          // u.batch(() => {
+          //   u.setScale('x', {
+          //     min: nxMin,
+          //     max: nxMax
+          //   })
+
+          //   u.setScale('y', {
+          //     min: nyMin,
+          //     max: nyMax
+          //   })
+          // })
         })
       }
     }
@@ -135,6 +157,7 @@ function makeChart() {
     width: 600,
     height: 400,
     plugins: [wheelZoomPlugin({ factor: 0.75 })],
+    // 视图中可见的最大最小范围
     scales: {
       x: {
         time: false
@@ -143,23 +166,19 @@ function makeChart() {
       //		auto: false,
       //	}
     },
+    // 数据集配置
     series: [
       {},
       {
         label: 'One',
         stroke: 'red'
-      },
-      {
-        label: 'Two',
-        stroke: 'blue'
       }
     ]
   }
 
   data.value = [
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    [40, 43, 60, 65, 71, 73, 80, 72, 84, 68],
-    [18, 24, 37, 55, 55, 60, 63, 65, 68, 65]
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+    [40, 43, 60, 65, 71, 73, 80, 72, 84, 68, 76, 84, 57]
   ]
 
   console.timeEnd('chart')
