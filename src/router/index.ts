159cd2d38302aa6a5b2ee5fd2d5192005c709663
jayawu <jayawu@dh-data.com>
2024-05-08 11:40:50 +0800

    fix:删除uplottest

--- src/router/index.ts
+++ src/router/index.ts
@@ -25,16 +25,6 @@ const router = createRouter({
       component: () => import('../views/UPlotZoom.vue')
     },
     {
-      path: '/uPlotTest',
-      name: 'uPlotTest',
-      component: () => import('../views/UPlotTest.vue')
-    },
-    {
-      path: '/uPlotTime',
-      name: 'uPlotTime',
-      component: () => import('../views/UPlotTime.vue')
-    },
-    {
       path: '/qrCode',
       name: 'qrCode',
       component: () => import('../views/QrCode.vue')
